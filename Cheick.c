02e315561a6999404ef47573903566ab9293d6cc
Cheick <cndiaye1234@gmail.com>
2018-08-23 13:16:09 -0500

    Added Source File
    Added Source File for Hack 01.
    (Cheick)

---
+++ Cheick.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Cheick
+ * Date: 08/23/2018
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Cheick and i'm a Computer Science major!\n");
+
+  return 0;
+}
